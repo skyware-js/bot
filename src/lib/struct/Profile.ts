665d9cc873e87fb9e434ad145f653de4e76d5b54
futurGH <me@futuristick.ca>
2024-02-16 18:51:36 -0500

    Simplify Profile class by basing it on ProfileViewDetailed rather than app.bsky.actor.profile record

--- src/lib/struct/Profile.ts
+++ src/lib/struct/Profile.ts
@@ -1,13 +1,13 @@
-import { BlobRef, ComAtprotoLabelDefs } from "@atproto/api";
+import { ComAtprotoLabelDefs } from "@atproto/api";
 import { Bot } from "../Bot";
 
 export interface ProfileData {
 	did: string;
-	handle?: string;
+	handle: string;
 	displayName?: string;
 	description?: string;
-	avatar?: BlobRef;
-	banner?: BlobRef;
+	avatar?: string;
+	banner?: string;
 	labels?: Array<ComAtprotoLabelDefs.Label>;
 }
 
@@ -20,7 +20,7 @@ export class Profile {
 	/**
 	 * The user's handle
 	 */
-	handle?: string;
+	handle: string;
 
 	/**
 	 * The user's display name
@@ -33,14 +33,14 @@ export class Profile {
 	description?: string;
 
 	/**
-	 * The user's avatar as a BlobRef
+	 * The user's avatar URL
 	 */
-	avatar?: BlobRef;
+	avatar?: string;
 
 	/**
-	 * The user's banner as a BlobRef
+	 * The user's banner URL
 	 */
-	banner?: BlobRef;
+	banner?: string;
 
 	/**
 	 * Labels on the user's profile
@@ -52,43 +52,24 @@ export class Profile {
 		public bot?: Bot,
 	) {
 		this.did = did;
-		if (handle) this.handle = handle;
+		this.handle = handle;
 		if (displayName) this.displayName = displayName;
 		if (description) this.description = description;
-		if (avatar) {
-			this.avatar = new BlobRef(avatar.ref, avatar.mimeType, avatar.size, avatar.original);
-		}
-		if (banner) {
-			this.banner = new BlobRef(banner.ref, banner.mimeType, banner.size, banner.original);
-		}
+		if (avatar) this.avatar = avatar;
+		if (banner) this.banner = banner;
 		this.labels = labels ?? [];
 	}
 
 	/**
-	 * Returns the user's avatar URL
+	 * Fetches a user's profile from their DID
+	 * @param did The user's DID
+	 * @param bot The Bot instance to use for the request
 	 */
-	get avatarUrl() {
-		if (!this.avatar) return null;
-		return `https://cdn.bsky.app/img/avatar/plain/${this.did}/${this.avatar.ref}@jpeg`;
-	}
-
-	/**
-	 * Returns the user's banner URL
-	 */
-	get bannerUrl() {
-		if (!this.banner) return null;
-		return `https://cdn.bsky.app/img/banner/plain/${this.did}/${this.banner.ref}@jpeg`;
-	}
-
-	/**
-	 * Resolves the user's handle and sets the handle property
-	 */
-	async resolveHandle() {
-		if (this.handle) return this.handle;
-		if (!this.bot) return null;
-		const repo = await this.bot.agent.com.atproto.repo.describeRepo({ repo: this.did });
-		if (!repo.success || !repo.data.handle || !repo.data.handleIsCorrect) return null;
-		this.handle = repo.data.handle;
-		return this.handle;
+	static async fromDid(did: string, bot: Bot): Promise<Profile> {
+		const profile = await bot.agent.getProfile({ actor: did });
+		if (!profile.success) {
+			throw new Error(`Failed to fetch profile ${did}\n` + JSON.stringify(profile.data));
+		}
+		return new Profile(profile.data, bot);
 	}
 }

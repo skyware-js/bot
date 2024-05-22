9e58ec0d320967c09ee452b1351739ac7fafa7a8
futurGH <me@futuristick.ca>
2024-05-22 16:37:57 -0400

    Have ChatMessage extend DeletedChatMessage

--- src/struct/chat/ChatMessage.ts
+++ src/struct/chat/ChatMessage.ts
@@ -6,19 +6,14 @@ import {
 import type { Bot, StrongRef } from "../../bot/Bot.js";
 import type { RichText } from "../../richtext/RichText.js";
 import { Facet } from "../post/Facet.js";
-import type { Profile } from "../Profile.js";
-import type { Conversation } from "./Conversation.js";
+import { DeletedChatMessage, type DeletedChatMessageData } from "./DeletedChatMessage.js";
 
 /**
  * Data used to construct a ChatMessage class.
  * @see ChatMessage
  */
-export interface ChatMessageData {
-	id: string;
+export interface ChatMessageData extends DeletedChatMessageData {
 	text: string;
-	conversationId?: string | undefined;
-	sender: { did: string };
-	sentAt: Date;
 	facets?: Array<Facet> | undefined;
 	embed?: StrongRef | undefined;
 }
@@ -26,21 +21,9 @@ export interface ChatMessageData {
 /**
  * Represents a message in a chat conversation.
  */
-export class ChatMessage {
-	/** The message's ID. */
-	id: string;
-
+export class ChatMessage extends DeletedChatMessage {
 	/** The message's text. */
 	text: string;
-
-	/** The ID of the conversation the message belongs to. */
-	conversationId?: string;
-
-	/** The DID of the message's sender. */
-	senderDid: string;
-
-	/** When the message was sent. */
-	sentAt: Date;
 
 	/** Annotations of text (mentions, URLs, hashtags, etc) */
 	facets?: Array<Facet>;
@@ -48,52 +31,21 @@ export class ChatMessage {
 	/** An embedded reference to a record. */
 	embed?: StrongRef;
 
-	/** The profile of the user who sent the message. */
-	private sender?: Profile;
-
-	/** The Conversation instance this message belongs to. */
-	private conversation?: Conversation;
-
 	/**
 	 * @param data Data used to construct the message.
 	 * @param bot The active Bot instance.
 	 */
-	constructor(
-		{ id, text, conversationId, sender, sentAt, facets, embed }: ChatMessageData,
-		protected bot: Bot,
-	) {
-		this.id = id;
+	constructor({ text, facets, embed, ...props }: ChatMessageData, bot: Bot) {
+		super(props, bot);
 		this.text = text;
-		if (conversationId) this.conversationId = conversationId;
-		this.senderDid = sender.did;
-		this.sentAt = sentAt;
 		if (facets) this.facets = facets;
 		if (embed) this.embed = embed;
 	}
 
 	/**
-	 * Fetch the profile of the user who sent this message.
-	 */
-	async getSender(): Promise<Profile> {
-		if (this.sender) return this.sender;
-		if (this.senderDid === this.bot.profile.did) return this.sender = this.bot.profile;
-		return this.sender = await this.bot.getProfile(this.senderDid);
-	}
-
-	/**
-	 * Fetch the Conversation instance this message belongs to.
-	 * Returns null if the conversation could not be found.
-	 */
-	async getConversation(): Promise<Conversation | null> {
-		if (this.conversation) return this.conversation;
-		if (!this.conversationId) return null;
-		return this.conversation = await this.bot.getConversation(this.conversationId);
-	}
-
-	/**
 	 * Constructs an instance from a MessageView.
 	 */
-	static fromView(
+	static override fromView(
 		view: ChatBskyConvoDefs.MessageView,
 		bot: Bot,
 		conversationId?: string,

cab6e90c91f6d2ae8bf87dace7555833a0ac101d
futurGH <me@futuristick.ca>
2024-10-27 16:02:54 -0400

    Fix link facet shortening skipping text following a link

--- src/richtext/detectFacets.ts
+++ src/richtext/detectFacets.ts
@@ -113,6 +113,7 @@ export function detectFacetsWithoutResolution(
 				text = text.slice(0, index.start) + shortenedUri + text.slice(index.end);
 				const lengthDifference = uri.length - shortenedUri.length;
 				index.end -= lengthDifference;
+				re.lastIndex -= lengthDifference;
 			}
 
 			facets.push({
